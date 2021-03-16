193f58ff60bcbe06b8344a8606e5633dc84286dd
Aditya Shah <nischalshah1999@gmail.com>
2021-03-16 13:08:58 +0545

    added onClick handlers

--- app/src/main/java/com/aditya/hellotoast/MainActivity.java
+++ app/src/main/java/com/aditya/hellotoast/MainActivity.java
@@ -2,13 +2,35 @@ package com.aditya.hellotoast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mshowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mshowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    public void countUp(View view) {
+        mCount++;
+        if (mshowCount != null) {
+            mshowCount.setText(Integer.toString(mCount));
+        }
     }
 }
