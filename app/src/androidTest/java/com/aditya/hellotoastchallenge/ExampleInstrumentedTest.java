7f824b35d4b3790096230489ec7fdbcd1dc684b4
Aditya Shah <nischalshah1999@gmail.com>
2021-03-16 14:06:57 +0545

    codingChallenge completed

--- app/src/androidTest/java/com/aditya/hellotoastchallenge/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/aditya/hellotoastchallenge/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.aditya.hellotoast;
+package com.aditya.hellotoastchallenge;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.aditya.hellotoast", appContext.getPackageName());
+        assertEquals("com.aditya.hellotoastchallenge", appContext.getPackageName());
     }
 }
